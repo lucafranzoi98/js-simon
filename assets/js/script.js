2d131f5619b0bb258f3544339070173e4f02bf91
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-05 14:30:12 +0200

    countdown in milliseconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 /* Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina! */
+
+const countdownEl = document.getElementById("countdown");
+
+const clock = setInterval(function(){
+   const date = new Date("09/06/2023 09:30:00");
+   const dateMilliseconds = date.getTime();
+   const currentTime = new Date();
+   const currentTimeMilliseconds = currentTime.getTime();
+   const finalTimeMilliseconds = dateMilliseconds - currentTimeMilliseconds;
+
+   if (finalTimeMilliseconds != 0) {
+      countdownEl.innerHTML = finalTimeMilliseconds;
+   } else {
+      clearInterval(clock);
+      countdownEl.innerHTML = "È arrivato il momento della correzione!";
+   }
+}, 1);
+
